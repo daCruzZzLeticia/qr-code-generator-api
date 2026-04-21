6b26093adbd45c6f58759c863642d178a949e58b
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2026-04-21 16:26:17 -0300

    feat: implementação do controller do qr code

---
+++ src/main/java/com/dacruzzzleticia/qrcode/generator/controller/QrCodeController.java
@@ -0,0 +1,33 @@
+package com.dacruzzzleticia.qrcode.generator.controller;
+
+import com.dacruzzzleticia.qrcode.generator.dto.QrCodeGenerateRequest;
+import com.dacruzzzleticia.qrcode.generator.dto.QrCodeGenerateResponse;
+import com.dacruzzzleticia.qrcode.generator.service.QrCodeGeneratorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/qrcode")
+public class QrCodeController {
+
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeService){
+        this.qrCodeGeneratorService = qrCodeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request){
+       try{
+           QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+           return ResponseEntity.ok(response);
+
+       } catch (Exception e){
+           return ResponseEntity.internalServerError().build();
+       }
+    }
+
+}
